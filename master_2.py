6ec4863137e2a07265a9be8020b980fd81fc8a7a
FinnLeh <31486031+FinnLeh@users.noreply.github.com>
2021-10-24 00:35:01 +0200

    Add files via upload

---
+++ master_2.py
@@ -0,0 +1,33 @@
+# In this task you will analyze basic data with pandas
+
+#Import the necessary libraries
+import pandas as pd
+
+# Assign it to a variable called users and use the 'user_id' as index
+users = pd.read_table('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user', sep='|', index_col='user_id')
+
+# Your tasks are below
+
+# Task1
+# See the first 25 entries
+
+# Task2
+# See the last 10 entries
+
+# Task3
+# What is the number of observations in the dataset?
+
+# Task4
+#What is the number of columns in the dataset?
+
+# Task5
+# What is the data type of each column?
+
+# Task 6
+# How many different occupations there are in this dataset?
+
+# Task 7
+# Summarize the DataFrame.
+
+# Task 8
+# Summarize all the columns
