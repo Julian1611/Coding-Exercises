92875c4040a7ff6b1da2039ad72ec847e9ec21fd
FinnLeh <finnleh5@gmail.com>
2021-10-24 13:29:31 +0200

    test change

--- Python/Intro-To-Programming/Introduction/intro_1.py
+++ Python/Intro-To-Programming/Introduction/intro_1.py
@@ -3,3 +3,4 @@ a = int(input("Please type your first number: "))
 b = int(input("Please type your second number: "))
 c = int(input("Please type your third number: "))
 print("The sum of your three numbers is: " + str(a+b+c))
+# code ends here
