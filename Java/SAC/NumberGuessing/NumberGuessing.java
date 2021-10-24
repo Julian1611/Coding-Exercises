bfd7e8d2ca217677effa1d5cbbb7aa1523ff6500
FinnLeh <finnleh5@gmail.com>
2021-10-25 02:22:31 +0300

    corrected for loop

--- Java/SAC/NumberGuessing/NumberGuessing.java
+++ Java/SAC/NumberGuessing/NumberGuessing.java
@@ -5,7 +5,7 @@ public class NumberGuessing {
     int i = UserInput.getInteger("Guess the number from 1 to 100, you have 10 chances: ");
     int mystery = (int)(Math.random() *100) + 1;
 
-    for (f = 0, f < 11, f++)
+    for (f = 0; f < 11; f++)
     {
       if (i != mystery)
       {
