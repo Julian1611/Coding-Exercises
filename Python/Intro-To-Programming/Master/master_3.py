f5fd5615eaf62af8aee75f526f59cceadfe98a90
FinnLeh <finnleh5@gmail.com>
2021-10-24 18:30:06 +0200

    master_3.py staged to Coding-Exercises/Python/Intro-To-Programming/Master

---
+++ Python/Intro-To-Programming/Master/master_3.py
@@ -0,0 +1,14 @@
+# In this task you will visualize your data
+# you should work with jupyter notebook! start it before doing the task.
+
+#Import the necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+%matplotlib inline
+
+# we will use data from Titanic Kaggle data set: https://raw.githubusercontent.com/mslyon/mariorepo/master/train.csv
+
+url = 'https://raw.githubusercontent.com/mslyon/mariorepo/master/train.csv'
