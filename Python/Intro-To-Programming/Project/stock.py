9cbd9dd40a6cfb25674dea018665e2d07e06a689
FinnLeh <finnleh5@gmail.com>
2021-11-01 15:34:57 +0100

    first proof of concept

--- Python/Intro-To-Programming/Project/stock.py
+++ Python/Intro-To-Programming/Project/stock.py
@@ -1,7 +1,15 @@
+# import all necessary modules
 import pdfplumber
+import sys
+import os
 
-statement = pdfplumber.open('/Statement of Securities Account.pdf')
+# open pdf to work with in current folder
+# file name could change, may need to be done by input by user
+statement = pdfplumber.open(os.path.join(sys.path[0], "Statement of Securities Account.pdf"))
 
-text = statement.extract_text()
-
-print(text)
+# get text for all pages in entire statement
+pages = statement.pages
+for i, pg in enumerate(pages):
+    page = statement.pages[i]
+    text = page.extract_text()
+    print(text)
