a65d7856e9eefe292d09e0d708902d4ebc3b885f
FinnLeh <finnleh5@gmail.com>
2021-11-10 14:46:12 +0100

    test main.py

--- Python/Intro-To-Programming/Project/main.py
+++ Python/Intro-To-Programming/Project/main.py
@@ -8,4 +8,18 @@
 
 import stock
 import PySimpleGUI as sg
-sg.Window(title="Stonks", layout=[[]], margins=(100, 50)).read()
+
+layout = [[sg.Text("This a test!")], [sg.Button("MONEY")]]
+
+# Create the window
+window = sg.Window("STONKS", layout, margins=(100,100))
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
